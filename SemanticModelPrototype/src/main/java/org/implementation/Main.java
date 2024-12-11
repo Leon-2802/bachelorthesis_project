46292dfb5a1522e282e745da8e5aa7f66fa3d8ea
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2024-12-11 22:29:41 +0100

    wip

--- SemanticModelPrototype/src/main/java/org/implementation/Main.java
+++ SemanticModelPrototype/src/main/java/org/implementation/Main.java
@@ -1,6 +1,7 @@
 package org.implementation;
 
 import org.apache.jena.ontapi.model.OntModel;
+import org.implementation.gtfs.GTFSLoader;
 import org.implementation.trias.TriasRequests;
 import org.implementation.trias.RequestTargets;
 import org.w3c.dom.Document;
@@ -8,14 +9,54 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        // load the OWL public transit ontology from file and intitialize the Ontology Service class:
-        OntModel om = HelperFunctions.loadModelFromFile("protege/public_transit_ontology.rdf");
-        runTriasExample(om);
-        // runGTFSExample();
-        SparQLService.sendQuery(om);
+//         load the OWL public transit ontology from file and intitialize the Ontology Service class:
+//        OntModel om = HelperFunctions.loadModelFromFile("protege/public_transit_ontology.rdf");
+//        runTriasExample(om);
+        runGTFSExample();
+//        SparQLService.sendQuery(om);
+    }
+
+    private static void runGTFSExample() {
+        // load the GTFS data from file and create XML trip data:
+        try {
+            Map<String, Map<String, String>> tripsData = GTFSLoader.parseGTFSFileToHashMap("data/fluo-grand-est-fluo67-gtfs/trips.txt", "trip_id");
+            Map<String, Map<String, String>> routesData = GTFSLoader.parseGTFSFileToHashMap("data/fluo-grand-est-fluo67-gtfs/routes.txt", "route_id");
+            Map<String, Map<String, String>> stopsData = GTFSLoader.parseGTFSFileToHashMap("data/fluo-grand-est-fluo67-gtfs/stops.txt", "stop_id");
+            List<Map<String, String>> transferData = GTFSLoader.parseGTFSFileToList("data/fluo-grand-est-fluo67-gtfs/transfers.txt", "to_stop_id");
+            List<Map<String, String>> stopTimesData = GTFSLoader.parseGTFSFileToList("data/fluo-grand-est-fluo67-gtfs/stop_times.txt", "trip_id");
+            // Print the parsed data
+            System.out.println("Parsed stop times data:");
+//            for (Map.Entry<String, Map<String, String>> entry : routesData.entrySet()) {
+//                System.out.println("Key: " + entry.getKey());
+//                for (Map.Entry<String, String> field : entry.getValue().entrySet()) {
+//                    System.out.println("  " + field.getKey() + ": " + field.getValue());
+//                }
+//            }
+//            for (Map<String, String> dataEntry : stopTimesData) {
+//                System.out.println("Map:");
+//                for (Map.Entry<String, String> entry : dataEntry.entrySet()) {
+//                    System.out.println("  " + entry.getKey() + ": " + entry.getValue());
+//                }
+//            }
+            Document testDoc = GTFSLoader.loadTripToXML("10", tripsData, stopTimesData, stopsData);
+            String testString = HelperFunctions.documentToString(testDoc);
+            System.out.println("Test XML Document:\n" + testString);
+        }
+        catch (IOException ex) {
+            System.out.println("IOException: " + ex.getMessage());
+        }
+        catch (IllegalArgumentException ex) {
+            System.out.println("IllegalArgumentException: " + ex.getMessage());
+        } catch (ParserConfigurationException ex) {
+            System.out.println("ParserConfigurationException: " + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Unexpected Exception occurred: " + ex.getMessage());
+        }
     }
 
     private static void runTriasExample(OntModel om) {
--- SemanticModelPrototype/src/main/java/org/implementation/gtfs/GTFSLoader.java
+++ SemanticModelPrototype/src/main/java/org/implementation/gtfs/GTFSLoader.java
@@ -1,11 +1,20 @@
-package org.implementation;
+package org.implementation.gtfs;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +26,62 @@ enum RouteType {
 public final class GTFSLoader {
     private GTFSLoader() {
         // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Parses a GTFS text file into a Map<String, Map<String, String>>.
+     *
+     * @param filePath The path to the GTFS text file.
+     * @param keyField The field name to use as the key for the outer map (e.g., "trip_id", "stop_id").
+     * @return A map representing the GTFS data.
+     * @throws IOException If there is an error reading the file.
+     */
+    public static Map<String, Map<String, String>> parseGTFSFileToHashMap(String filePath, String keyField) throws IOException, IllegalArgumentException {
+        Map<String, Map<String, String>> dataMap = new HashMap<>();
+
+        try (Reader reader = new FileReader(filePath)) {
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+
+            // Get the header map to find the key field index
+            Map<String, Integer> headerMap = csvParser.getHeaderMap();
+            if (!headerMap.containsKey(keyField)) {
+                throw new IllegalArgumentException("Key field not found in the header: " + keyField);
+            }
+
+            for (CSVRecord csvRecord : csvParser) {
+                String key = csvRecord.get(keyField);
+                Map<String, String> fieldMap = new HashMap<>();
+                for (Map.Entry<String, Integer> header : headerMap.entrySet()) {
+                    fieldMap.put(header.getKey(), csvRecord.get(header.getValue()));
+                }
+                dataMap.put(key, fieldMap);
+            }
+        }
+
+        return dataMap;
+    }
+
+    public static List<Map<String, String>> parseGTFSFileToList(String filePath, String keyField) throws IOException, IllegalArgumentException{
+        List<Map<String, String>> dataList = new ArrayList<>();
+        try (Reader reader = new FileReader(filePath)) {
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+
+            // Get the header map to find the key field index
+            Map<String, Integer> headerMap = csvParser.getHeaderMap();
+            if (!headerMap.containsKey(keyField)) {
+                throw new IllegalArgumentException("Key field not found in the header: " + keyField);
+            }
+
+            for (CSVRecord csvRecord : csvParser) {
+                Map<String, String> fieldMap = new HashMap<>();
+                for (Map.Entry<String, Integer> header : headerMap.entrySet()) {
+                    fieldMap.put(header.getKey(), csvRecord.get(header.getValue()));
+                }
+                dataList.add(fieldMap);
+            }
+        }
+
+        return dataList;
     }
     /**
      * Loads GTFS data for a specific trip into an XML document.
